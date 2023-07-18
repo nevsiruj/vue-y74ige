98f87e884a67c8c2bd71b7a5e78791a94ad69a60
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-07-18 10:37:39 -0300

    1 file modified

--- src/router.js
+++ src/router.js
@@ -3,8 +3,8 @@ import HelloWorld from './components/HelloWorld.vue';
 import LandingPromoEmprendedor from './components/landing/LandingPromoEmprendedor.vue';
 
 const routes = [
-  { path: '/hello', component: HelloWorld },
-  { path: '/', component: LandingPromoEmprendedor },
+  { path: '/', component: HelloWorld },
+  { path: '/hello', component: LandingPromoEmprendedor },
 ];
 
 const router = createRouter({
